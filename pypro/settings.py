d9a6926e505fb716298c0a1296d38c2d9b91440b
r-trevisan <rrhtt3@gmail.com>
2022-02-11 22:15:06 -0400

    Coleta de Arquivos Estáticos
    close #26

--- pypro/settings.py
+++ pypro/settings.py
@@ -121,7 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Configuração de Ambiente de Desenvolvimento
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
